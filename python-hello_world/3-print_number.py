e70339a886845529c5c64eaee3547a33ea552ba2
Micke2310 <maikyarachan@gmail.com>
2024-05-16 00:11:51 +0000

    hello-

---
+++ python-hello_world/3-print_number.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""This script prints an integer followed by 'Battery street'."""
+number = 98
+print(f"{number} Battery street")
---
+++ python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""This script prints a float with 2 digits of precision."""
+number = 3.14159
+print(f"Float: {number:.2f}")
