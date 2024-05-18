eccedf63804753b9b734b14b6febe0c4b118b1c6
Micke2310 <maikyarachan@gmail.com>
2024-05-18 03:39:30 +0000

    hello

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-#!/usr/bin/python3
 print((("object" + "-" + "oriented") + " " + "programming") + " " + "with" + " " + "Python")
