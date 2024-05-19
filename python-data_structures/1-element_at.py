af571b364c3b25091c49a049d203e236a3c6c6bc
Micke2310 <maikyarachan@gmail.com>
2024-05-19 22:20:33 +0000

    data-struc

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    my_list = [1, 2, 3, 4, 5]
+    idx = 0
+
+    if idx < 0: 
+    print("None")
+
+    elif idx => len(my_list):
+    print("None")
+
+    else 
+    return my_list[idx]
