ba8ea20614fa69f3ff623ff52b74b991134e55d5
Micke2310 <maikyarachan@gmail.com>
2024-05-20 00:40:09 +0000

    .

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,10 +2,7 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    if my_list != None:
+    if my_list != None:  # noqa:E711
 
         for num in range(len(my_list) - 1, -1, -1):
-            #el primer -1 del rango marca el inicio de la iteracion
-            #(el ultimo elemento), el segundo marca el final (llega hasta el indice 0),
-            #y el tercero marca el paso a paso (de 1 en 1 hacia atras).
             print("{:d}".format(my_list[num]))
