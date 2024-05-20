a2b20caba93af66d5363c8a2806597630ff6ae12
Micke2310 <maikyarachan@gmail.com>
2024-05-20 02:25:49 +0000

    data-struc

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for sub_matrix in matrix:
+        for element in sub_matrix:
+            print("{:d}".format(element), end="")
+        print()
