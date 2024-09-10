abe03e6486a27165e6f970dcc3b69af7cc2f67d6
Micke2310 <maikarachan@gmail.com>
2024-09-10 04:30:08 +0000

    string

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str}{str}{str} \n")
+print(f"{str}{str}{str}")
 the_first_9_str = str [:-7]
-print(f"{the_first_9_str} \n")
+print(f"{the_first_9_str}")
