778161889a9cc5ecf79963a8f8045ac1bfcb43f7
Micke2310 <maikyarachan@gmail.com>
2024-09-12 05:22:27 +0000

    7 islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,20 @@
-/usr/bin/python3
+#!/usr/bin/python3
+
+
+#def islower(caracter):la 'c' es muetra de un caracter en minuscula "c" o "C"
+                    #es el parametro la variable que se define en la funcion,
+                    #cuando le asigno un valor a la funcion es el argumento.
+def islower(caracter):
+    if caracter >= 'a' and caracter <= 'z':
+        return True
+    else:
+        return False
+
+
+islower = __import__('7-islower').islower
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
