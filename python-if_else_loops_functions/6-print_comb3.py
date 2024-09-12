62a69bbb0884b611790c45cd2976eca1c49888dc
Micke2310 <maikyarachan@gmail.com>
2024-09-12 04:17:35 +0000

    6print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,10 +3,10 @@
 A = range(0, 10)
 B = range(0, 10)
 
-for a in A :
+for a in A:
     for b in B:
         if a < b:
             if a == 8 and b == 9:
                 print("{}{}".format(a, b))
-            else: 
+            else:
                 print("{}{}, ".format(a, b), end="")
