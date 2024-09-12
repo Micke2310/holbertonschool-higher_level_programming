bd565e9f5e44b9506729d0d4395e85a0cc00ccde
Micke2310 <maikyarachan@gmail.com>
2024-09-12 00:35:24 +0000

    3print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,7 @@
-/usr/bin/python3
+#!/usr/bin/python3
+num = 0
+for num in range(97, 123):
+    if num == 101 or num == 113:
+        continue
+    alphabet = chr(num)
+    print("{}".format(alphabet), end="")
