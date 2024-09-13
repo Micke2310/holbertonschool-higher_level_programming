ad249a08a01c29af60bf412968cde684e7ec29ac
Micke2310 <maikyarachan@gmail.com>
2024-09-13 20:26:14 +0000

    ..

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-
+'''
 def islower(c):
     # Verifica si el valor ASCII del carácter está en el rango de letras minúsculas
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
+        '''
+
+def islower(caracter):
+        if ord(caracter) >= 97 and ord(caracter) <= 123:
+            return True
+        else:
+            return False
 
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
