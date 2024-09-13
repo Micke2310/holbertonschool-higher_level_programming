4070f869531e030520f97333fd82bb645a36714a
Micke2310 <maikyarachan@gmail.com>
2024-09-13 20:40:41 +0000

    ..

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,18 +2,7 @@
 
 
 def islower(caracter):
-        if ord(caracter) >= 97 and ord(caracter) <= 123:
-            return True
-        else:
-            return False
-'''
-#!/usr/bin/python3
-def islower(c):
-    c = ord(c)
-    if c >= ord('a') or c >= ord('z'):
+    if ord(caracter) >= 97 and ord(caracter) <= 123:
         return True
-    elif c >= ord('Z') or c>= ord('Z'):
-        return False
     else:
         return False
-    '''
