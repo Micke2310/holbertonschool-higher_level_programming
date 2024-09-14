e1b1f80b1be8b85fe2ea80dbbc5ee7091ec84a20
Micke2310 <maikyarachan@gmail.com>
2024-09-14 01:19:11 +0000

    -.-.

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def uppercase(string):
-    if string in range("a","z")
-        string =
+    resultado = ""
+
+    for char in string:
+        if char in range("a","z"):
+            resultado = resultado + chr(ord(char) -32)
+
+        else:
+            resultado = resultado + char
+
+    return resultado
