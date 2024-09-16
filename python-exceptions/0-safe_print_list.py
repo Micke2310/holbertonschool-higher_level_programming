9153a5589b044e491e96ff5a1280f6ae81396636
Micke2310 <maikyarachan@gmail.com>
2024-09-16 02:39:22 +0000

    0safe

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-            count += 1
+            contador += 1
     except IndexError:
         pass
     finally:
         print("")
-    return count
+    return contador
