435d0471a6444033d2081b1f735f7647f2845484
Micke2310 <maikyarachan@gmail.com>
2024-09-16 01:23:17 +0000

    2remplace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,7 +4,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        print(my_list)
         my_list[idx] = element
         new_list = my_list
         return new_list 
