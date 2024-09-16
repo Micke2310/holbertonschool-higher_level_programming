1936e60fb610001d7cc07f0ba551e016f7bc6ee3
Micke2310 <maikyarachan@gmail.com>
2024-09-16 01:20:44 +0000

    2remplace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    new_list = []
+
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         print(my_list)
         my_list[idx] = element
         new_list = my_list
-        return 
+        return new_list 
