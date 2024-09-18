3c85ffec8e3ba59b455006b1b87eaca2830b3851
Micke2310 <maikyarachan@gmail.com>
2024-09-18 16:17:45 +0000

    .

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -5,6 +5,6 @@ def no_c(my_string):
 
     while i < len(my_string):
         if my_string[i] != 'c' and my_string[i] != 'C':
-            new_string = new_string + my_string[]
+            new_string = new_string + my_string[i]
         i= i + 1
     return new_string
