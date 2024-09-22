58b01364d0e547ef595632e32b1b02e05c7eb0e4
Micke2310 <mickearachan@gmail.com>
2024-09-22 04:22:39 +0000

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,14 @@
-/usr/bin/python3
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = 0
+    first = None
+
+        if sentence:
+            length = len(sentence)
+            first = sentence[0]
+            print(" Lenght: {:d} - First: {}".format(lenght, first))
+        else:
+            lenght = 0
+            first = None
+        return
+        
