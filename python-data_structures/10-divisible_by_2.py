455062572943bfe3c583cc4ee0101770efc7fa7d
Micke2310 <mickearachan@gmail.com>
2024-09-22 05:24:20 +0000

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
-/usr/bin/python3
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    resultado = []
+    for numeros in my_list:
+        if numeros %2 == 0:
+            resultado.append(True)
+        else:
+            resultado.append(False)
+    return resultado
