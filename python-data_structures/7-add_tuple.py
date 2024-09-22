2eeee2728f86ddcbb07438d9be8aaf1449492ad5
Micke2310 <mickearachan@gmail.com>
2024-09-22 03:41:51 +0000

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a = (), tuple_b = ()):
-    a1, a2 = (tupla_a + (0, 0))[:2]
-    b1, b2 = (tupla_b + (0, 0))[:2]
+    a1, a2 = (tuple_a + (0, 0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
 
     suma1 = a1 + b1
     suma2 = a2 + b2
