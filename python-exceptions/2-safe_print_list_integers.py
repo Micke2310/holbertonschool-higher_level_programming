0da991de4b5d711696af38fb4dd093a15a412797
Micke2310 <maikyarachan@gmail.com>
2024-09-30 02:08:25 +0000

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,8 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(dato), end="")
                 contador = contador + 1
 
-        except IndexError:
+        except IndexError as e:
+            print(e)
             break
 
     print ("")
