dbb3be4264829cea734296bc38979f3cb15f4d3f
Micke2310 <maikyarachan@gmail.com>
2024-09-30 03:08:16 +0000

    ok

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     resultado = []
 
     for i in range(list_length):
-        
+
         try:
             a = my_list_1[i]
             b = my_list_2[i]
