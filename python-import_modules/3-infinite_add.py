e9e650d8873cf84ed06ae88681d94444706a5ee9
Micke2310 <maikyarachan@gmail.com>
2024-10-15 17:12:33 +0000

    ...

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
-/usr/bin/python3
+#!/usr/bin/python3
+import sys
+suma = 0
+
+for i in sys.argv[1:]:
+    suma = suma + int(i)
+
+print("{}".format(suma))
--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,1 +1,4 @@
-/usr/bin/python3
+#!/usr/bin/python3
+from variable_load_5 import a
+valor = a
+print("{}".format(valor))
