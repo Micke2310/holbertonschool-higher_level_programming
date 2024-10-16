607609147348d505a9542abaafd616c5240bd56c
Micke2310 <maikyarachan@gmail.com>
2024-10-16 14:34:25 +0000

    square

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def square_matrix_simpl(matrix=[]):
+    new_matrix = [] 
+    
+    for fila in matrix:
+        new_fila = []
+        
+        for elemento in fila:
+            cuadrado = elemento * elemento
+            new_fila.append(cuadrado)
+
+        new_matrix.append(new_fila)
+    return new_matrix
