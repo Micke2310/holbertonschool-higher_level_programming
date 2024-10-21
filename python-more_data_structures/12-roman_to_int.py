152f9bda666d5dd29d066f056b11c1f06ea0f1ef
Micke2310 <maickyarachan@gmail.com>
2024-10-21 14:48:25 +0000

    pp

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def roman_to_int(s):
+
+    diccionario_romano = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    resultado = None
+    valor_ant = None
+
+    for i in range(len(s) -1, -1, -1):
+        valor_actual = diccionario_romano[s[i]]
+        if valor_actual >= valor_ant:
+            resultado += valor_actual
+
+        else:
+            resultado -= valor_actual
+            valor_ant = valor_actual
+
+    return resultado
