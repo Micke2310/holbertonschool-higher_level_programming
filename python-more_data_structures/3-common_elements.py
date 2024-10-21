5fc711c1a987939d81f9534c3abc677e85559e49
Micke2310 <maikyarachan@gmail.com>
2024-10-21 01:11:05 +0000

    mmm

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -2,7 +2,7 @@
 
 def common_elements(set_1, set_2):
     '''return set_1 & set_2'''
-    set_inter = set ()
+    set_inter = set()
     for i in set_1:
         for j in set_2:
             if i == j:
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,7 @@ def only_diff_elements(set_1, set_2):
     '''return set_1 ^ set_2'''
     set_3_diff = set()
 
-    for i en set_1:
+    for i in set_1:
         for j in set_2:
             if i != j:
                 set_3_diff.append(i)
