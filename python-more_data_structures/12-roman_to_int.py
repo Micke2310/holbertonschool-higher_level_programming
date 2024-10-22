debcceadf5105549705e618a7ac2e1028030d610
Micke2310 <maikyarachan@gmail.com>
2024-10-22 16:40:48 +0000

    l

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(s):
     if not isinstance(s, str):
         return 0
 
-    diccionario_romano = {'I': 1, 'V': 5,
+    diccionario_romano = {'I': 1, 'V': 5,\
     'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     resultado = 0
     valor_ant = 0
