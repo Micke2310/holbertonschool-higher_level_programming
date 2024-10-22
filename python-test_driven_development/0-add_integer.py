4a0ef93ea44700006ff1a188df8e1d103cd90f37
Micke2310 <maikyarachan@gmail.com>
2024-10-22 21:22:04 +0000

    --

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,21 @@
-/usr/bin/python3
+#!/usr/bin/python3
+def add_integer(a, b=98):
+''' suma de dos enteros
+a = debe ser un valor entero
+b = debe de ser un valor entero
+
+en caso que los valores sean flotantes convertirlos en enteros y en caso de no ser
+entero o flotante, manejarlo como errores.
+
+retornar: suma de a y b
+'''
+suma = int(a) + int(b)
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a and b must be integers or floats")
+
+        a = int(a)
+
+        b = int(b)
+
+    return suma
