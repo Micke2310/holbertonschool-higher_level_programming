6b31ccd1dfa9291b2c7d49f5c448fbd0801fccdb
Micke2310 <maikyarachan@gmail.com>
2024-10-22 22:16:42 +0000

    --

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-'''
-suma de dos enteros
-a = debe ser un valor entero
-b = debe de ser un valor entero
+    '''
+    suma de dos enteros
+    a = debe ser un valor entero
+    b = debe de ser un valor entero
 
-en caso que los valores sean flotantes convertirlos en enteros y en caso de no ser
-entero o flotante, manejarlo como errores.
+    en caso que los valores sean flotantes convertirlos en enteros y en caso de no ser
+    entero o flotante, manejarlo como errores.
 
-retornar: suma de a y b
-'''
+    retornar: suma de a y b
+    '''
 suma = int(a) + int(b)
 
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
