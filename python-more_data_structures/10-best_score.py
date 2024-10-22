06501fd41cf0b476882b3b28bb95d3800f5df6b7
Micke2310 <maikyarachan@gmail.com>
2024-10-22 15:56:46 +0000

    l

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,9 +4,10 @@ def best_score(a_dictionary):
 
 
     if not a_dictionary:
-        max_value = max(a_dictionary.values())
         return None
-    
+
+    max_value = max(a_dictionary.values())
+
     for key, value in a_dictionary.items():
         if value == max_value:
             return key
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,16 +3,16 @@
 def roman_to_int(s):
 
     diccionario_romano = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    resultado = None
-    valor_ant = None
+    resultado = 0
+    valor_ant = 0
 
-    for i in range(len(s) -1, -1, -1):
-        valor_actual = diccionario_romano[s[i]]
-        if valor_actual >= valor_ant:
-            resultado += valor_actual
+    for i in reversed(s)
+        valor_actual = diccionario_romano[i]
+        if valor_actual > valor_ant:
+            resultado -= valor_actual
 
         else:
-            resultado -= valor_actual
+            resultado += valor_actual
             valor_ant = valor_actual
 
     return resultado
