e7ec914df9acda54173b282503e93e7af162c673
Micke2310 <maikyarachan@gmail.com>
2024-10-25 05:22:54 +0000

    -

--- restful-api/main_02_requests.py
+++ restful-api/main_02_requests.py
@@ -4,8 +4,8 @@ import csv
 def mostrar_posts_jsonplaceholder_api():
     respuesta = requests.get('https://jsonplaceholder.typicode.com/posts')
     if respuesta.status_code == 200:
-        posts = respuesta.json()
-        for i in posts:
+        lista_posts = respuesta.json()
+        for i in lista_posts:
             print(i['title'])
 
         else:
@@ -15,8 +15,12 @@ def mostrar_posts_jsonplaceholder_api():
 def guardar_posts_csv_jsonplaceholder_api():
     respuesta = requests.get('https://jsonplaceholder.typicode.com/posts')
     if respuesta.status_code == 200:
-        psts = respuesta.json()
+        lista_posts = respuesta.json()
         with open('posts.csv', 'w', newlline= '') as csvfile:
+            columnas_archivo_csv = ['id', 'title', 'body']
+            objeto_generador_datos_csv = csv.DictWriter(csvfile, fieldnames = columnas_archivo_csv)
+            objeto_generador_datos_csv.writeheader()
+            objeto_generador_datos_csv.writerows(lista_post)
 
-
-
+        else:
+            print(f"Error: {respuesta.status_code}")
