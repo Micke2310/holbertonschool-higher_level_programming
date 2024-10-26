e3406e3b8a6a8c59fd34d04f97eb40cb97e8394d
Micke2310 <maikyarachan@gmail.com>
2024-10-26 00:44:52 +0000

    -

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,30 +1,31 @@
+#!/usr/bin/python3
+
 import requests
 import csv
 
-def obtener_y_mostrar_posts():
-    """Obtiene los posts de la API JSONPlaceholder y muestra sus títulos."""
+def mostrar_posts_jsonplaceholder_api():
     respuesta = requests.get('https://jsonplaceholder.typicode.com/posts')
     if respuesta.status_code == 200:
-        posts = respuesta.json()
-        for post in posts:
-            print(post['title'])
-    else:
-        print(f"Error al obtener los posts: {respuesta.status_code}")
+        lista_posts = respuesta.json()
+        for i in lista_posts:
+            print(i['title'])
 
-def guardar_posts_en_csv():
-    """Obtiene los posts y los guarda en un archivo CSV."""
+        else:
+            print(f"Error: {respuesta.status_code}")
+
+
+def guardar_posts_csv_jsonplaceholder_api():
     respuesta = requests.get('https://jsonplaceholder.typicode.com/posts')
     if respuesta.status_code == 200:
-        posts = respuesta.json()
+        lista_posts = respuesta.json()
+        with open('posts.csv', 'w', newline= '') as csvfile:
+            columnas_archivo_csv = ['userId', 'id', 'title', 'body']
+            objeto_generador_datos_csv = csv.DictWriter(csvfile, fieldnames = columnas_archivo_csv)
+            objeto_generador_datos_csv.writeheader()
+            objeto_generador_datos_csv.writerows(lista_posts)
+        print("Datos guardados en lista_posts")
 
-        with open('posts.csv', 'w', newline='', encoding='utf-8') as archivo_csv:
-            campos = ['userId', 'id', 'title', 'body']
-            escritor_csv = csv.DictWriter(archivo_csv, fieldnames=campos)
-            escritor_csv.writeheader()
-            escritor_csv.writerows(posts)
-        print("Datos guardados en posts.csv")
     else:
-        print(f"Error al guardar los posts: {respuesta.status_code}")
+        print(f"Error: Al guardar lista_posts. {respuesta.status_code}")
 
-# Llamar a la función para guardar los posts
-guardar_posts_en_csv()
+guardar_posts_csv_jsonplaceholder_api()
