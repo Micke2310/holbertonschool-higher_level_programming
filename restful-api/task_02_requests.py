0b2124d5586383cfb87a3a6af6fb77bae0294f3a
Micke2310 <maikyarachan@gmail.com>
2024-10-26 02:04:56 +0000

    -

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,7 +5,9 @@ import csv
 
 def fetch_and_print_posts():
     respuesta = requests.get('https://jsonplaceholder.typicode.com/posts')
-    if respuesta.status_code == 200:
+    print(f"Status Code: {respuesta.status_code}")
+
+if respuesta.status_code == 200:
         lista_posts = respuesta.json()
         for i in lista_posts:
             print(i['title'])
